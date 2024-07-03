d13e6344b62ea96750038e17e5046f5d89adfb84
chauser6 <carlena.hauser1108@gmail.com>
2024-07-02 20:45:54 -0400

    Change update_item endpoint from GET to PUT method  - Correct the HTTP method for update_item endpoint from GET to PUT. - The PUT method is appropriate for updating resources, aligning with RESTful conventions. - Define Item model using Pydantic with name, price, and optional is_offer fields. - Implement update_item endpoint to update and return item details based on the provided item_id and item data.

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-
 app = FastAPI()
+
 
 class Item(BaseModel):
     name: str
@@ -16,10 +16,12 @@ class Item(BaseModel):
 def read_root():
     return {"Hello": "World"}
 
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
-@app.get("/items/{item_id}")
+
+@app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_id": item.name, "item": item}
